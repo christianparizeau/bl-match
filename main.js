c4733bb1033a751b3a062dbff0362e1547ebdbe3
Doctorbingbing <christianparizeau@gmail.com>
2019-10-01 17:18:09 -0700

    Matching cards stay and non-matching pairs are flipped after 1.5 seconds

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 $(document).ready(init)
-
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
 function init(){
-    $('.card-back').on('click', handleCardClick)
+    $('.gameboard').on('click','.card-back',handleCardClick)
 }
 
 function handleCardClick(event){
-    $(event.currentTarget).toggleClass('hidden');
+    $(this).toggleClass('hidden');
+    if(!firstCardClicked){
+        firstCardClicked= $(this);
+        return
+    }
+    else{
+        secondCardClicked = $(this);
+      
+        if(firstCardClicked[0].nextSibling.className === secondCardClicked[0].nextSibling.className){
+            console.log("It's a match!");
+            matches++;
+            resetCards();
+        }
+        else{
+            setTimeout(function(){
+                firstCardClicked.toggleClass('hidden');
+                secondCardClicked.toggleClass('hidden');
+                resetCards();            
+            },1500)
+            
+        }
+        
+
+    }
+    
 }
+
+function resetCards(){
+    firstCardClicked = null;
+    secondCardClicked = null;
+}
+
